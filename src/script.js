17d3fb8bd5e123c041a37d5c063dc5893ee7225b
tirzahe <tirzah410@gmail.com>
2017-11-16 21:06:54 -0700

    testing processNumber & processOperator function

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
-function add(a, b) {
-  return a + b;
+function addToScreen(input) {
+    document.getElementById('screen').innerText += input;
+}
+function setScreen(input){
+    document.getElementById('screen').innerText = input;
+}
+function processNumber(input) {
+  addToScreen(input);
+}
+function processOperator(input) {
+  setScreen(input);
 }
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,37 @@
-describe('Test', function() {
-  describe('add', function() {
-    it('should add two numbers', function() {
-      expect(add(2, 3)).toBe(5);
+describe('Calculator', function() {
+  // describe('addToScreen', function() {
+  //   it('should display input in screen', function() {
+  //     var screen = document.createElement('div');
+  //     // document.getElementById = jasmine.createSpy().and.returnValue(screen);
+  //     spyOn(document, 'getElementById').and.returnValue(screen);
+  //     spyOn(window, 'addToScreen');
+  //     addToScreen('3');
+  //     expect(document.getElementById).toHaveBeenCalledWith('screen');
+  //     expect(screen.innerText).toBe('3');
+  //   });
+  // });
+  describe('processNumber', function() {
+    it('should display input in screen', function() {
+      spyOn(window, 'addToScreen').and.returnValue(null);
+      processNumber(3);
+      expect(addToScreen).toHaveBeenCalledWith(3);
+    });
+  });
+  describe('processOperator', function(){
+    it('should add operator after number', function() {
+      spyOn(window, 'addToScreen').and.returnValue(null);
+      processOperator('+');
+      expect(addToScreen).toHaveBeenCalledWith('+');
+    });
+    it('should replace last character if operator', function() {
+      var screen = document.createElement('div');
+      screen.setAttribute('id', 'screen');
+      document.body.appendChild(screen);
+      spyOn(window, 'setScreen').and.returnValue(null);
+      processNumber(0);
+      processOperator('+');
+      processOperator('-');
+      expect(setScreen).toHaveBeenCalledWith('0-');
     });
   });
 });
