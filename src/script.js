9a2013cc5f95b3b5305ac162c39360b580635623
tirzahe <tirzah410@gmail.com>
2017-11-17 11:45:12 -0700

    changed button tag to input tag in HTML

--- src/script.js
+++ src/script.js
@@ -10,3 +10,9 @@ function processNumber(input) {
 function processOperator(input) {
   setScreen(input);
 }
+function clear(){
+  //this function will clear the last button pressed from the screen
+}
+function allClear(){
+  //this function will clear and reset all functioms to zero.
+}
